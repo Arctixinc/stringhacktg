ea3c3a4cf8a42457efbf2f1e35d12d433e6454f3
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-06-07 19:07:49 +0530

    Update steve.py

--- DAXXSTRINGHACK/Helpers/steve.py
+++ DAXXSTRINGHACK/Helpers/steve.py
@@ -48,9 +48,9 @@ async def user_info(session):
     err = ""
     try:
         if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
+            steve = TelegramClient(StringSession(session), API_ID, API_HASH)
             await steve.connect()
-            k = await steve.get_me()  
+            k = await steve.get_me()
             msg = info.format(
                 k.first_name if k.first_name else k.last_name,
                 k.id,
@@ -59,23 +59,22 @@ async def user_info(session):
                 k.username
             )
             await steve.disconnect()
-                             
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+        else:
+            async with Client("stark", api_id=API_ID, api_hash=API_HASH, session_string=session) as stark:
                 k = await stark.get_me()
                 msg = info.format(
-                k.first_name if k.first_name else k.last_name,
-                k.id,
-                k.id,
-                k.phone,
-                k.username
-            )
+                    k.first_name if k.first_name else k.last_name,
+                    k.id,
+                    k.id,
+                    k.phone_number,
+                    k.username
+                )
     except Exception as idk:
         err += str(idk)
-                    
+
     if err:
         return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return msg    
+    return msg
 
 RIGHTS = ChatBannedRights(
     until_date=None,
