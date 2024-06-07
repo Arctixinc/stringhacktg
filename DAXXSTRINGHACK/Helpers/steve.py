dd1a7cdc77b789c9f3b7003ff51532a36a34740e
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-06-07 18:22:30 +0530

    Update steve.py

--- DAXXSTRINGHACK/Helpers/steve.py
+++ DAXXSTRINGHACK/Helpers/steve.py
@@ -43,27 +43,41 @@ async def users_gc(session):
     if err:
         return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
     return msg
- 
+
 async def user_info(session):
     err = ""
     try:
         if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
+            steve = TelegramClient(StringSession(session), API_ID, API_HASH)   
             await steve.connect()
             k = await steve.get_me()  
-            msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone,k.username)
+            msg = info.format(
+                k.first_name if k.first_name else k.last_name,
+                k.id,
+                k.phone,
+                k.username,
+                k.first_name if k.first_name else k.last_name,
+                k.id
+            )
             await steve.disconnect()
-                             
         else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+            async with Client("stark", api_id=API_ID, api_hash=API_HASH, session_string=session) as stark:
                 k = await stark.get_me()
-                msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone_number,k.username)
+                msg = info.format(
+                    k.first_name if k.first_name else k.last_name,
+                    k.id,
+                    k.phone_number,
+                    k.username,
+                    k.first_name if k.first_name else k.last_name,
+                    k.id
+                )
     except Exception as idk:
         err += str(idk)
                     
     if err:
         return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return msg    
+    return msg
+
 
 RIGHTS = ChatBannedRights(
     until_date=None,
